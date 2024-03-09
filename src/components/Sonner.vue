2117f2d569f7f4dfd4df49e90efc2f63b750ff2d
Kasparas-G55 <kasparas.galdikas55@gmail.com>
2024-03-09 19:54:41 +0200

    feat: add `vue-sonner` component credits: `shadcn/vue` and `vue-sonner`

---
+++ src/components/Sonner.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import { Toaster as Sonner, type ToasterProps } from "vue-sonner";
+
+const props = defineProps<ToasterProps>();
+</script>
+
+<template>
+  <Sonner
+    class="toaster group"
+    v-bind="props"
+    :toast-options="{
+      classes: {
+        title: 'group toast group-[.toaster]:text-white',
+        toast: 'group toast group-[.toaster]:bg-gray-700 group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg',
+      },
+    }"
+  />
+</template>
