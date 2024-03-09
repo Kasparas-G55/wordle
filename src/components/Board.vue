a79859b0141f572457eca9bd2bce343fbc20eab5
Kasparas-G55 <kasparas.galdikas55@gmail.com>
2024-03-09 20:05:24 +0200

    feat: color board cells if a letter matches or exists in the word

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -1,22 +1,59 @@
 <template>
   <div class="flex flex-col gap-2 pb-4">
     <BoardRow
-      v-for="(row, i) of 6"
-      :key="row"
-      :data-test="guesses[i]"
-      :guess="guesses[i]"
-    />
+      v-for="(row, rowIndex) of 6"
+      :key="`row-${row}`"
+      :data-test="guesses[rowIndex]"
+    >
+      <BoardCell
+        v-for="(cell, cellIndex) of WORD_SIZE"
+        :key="`cell-${cell}`"
+        :class="getCellColor(rowIndex, cellIndex)"
+      >
+        {{ getCellLetter(rowIndex, cellIndex) }}
+      </BoardCell>
+    </BoardRow>
   </div>
 </template>
 
 <script setup lang="ts">
+import { computed } from "vue";
 import type { PropType } from "vue";
+import { GUESS_WORD, WORD_SIZE } from "@/settings";
+import BoardCell from "./BoardCell.vue";
 import BoardRow from "./BoardRow.vue";
 
-defineProps({
+const props = defineProps({
   guesses: {
     type: Array as PropType<string[]>,
     required: true,
   },
+  attempts: {
+    type: Number,
+    required: true,
+  },
 });
+
+const guess = computed(() => props.guesses[props.attempts]);
+
+// TODO: Code Optimization
+function getCellColor(rowIndex: number, cellIndex: number) {
+  if (rowIndex >= props.attempts || !props.guesses[rowIndex])
+    return;
+
+  const letters = [...props.guesses[rowIndex]];
+
+  if (GUESS_WORD[cellIndex] === letters[cellIndex])
+    return "bg-green-600";
+
+  if (GUESS_WORD.includes(letters[cellIndex]))
+    return "bg-yellow-500";
+}
+
+function getCellLetter(rowIndex: number, cellIndex: number) {
+  if (props.attempts !== rowIndex)
+    return props.guesses[rowIndex]?.[cellIndex] ?? "";
+
+  return guess.value[cellIndex];
+}
 </script>
--- src/components/BoardRow.vue
+++ src/components/BoardRow.vue
@@ -1,22 +1,5 @@
 <template>
   <div class="flex flex-row gap-2">
-    <BoardLetter
-      v-for="(num, i) of WORD_SIZE"
-      :key="`letter-${num}`"
-    >
-      {{ guess ? guess[i] : "" }}
-    </BoardLetter>
+    <slot />
   </div>
 </template>
-
-<script setup lang="ts">
-import { WORD_SIZE } from "@/settings";
-import BoardLetter from "./BoardLetter.vue";
-
-defineProps({
-  guess: {
-    type: String,
-    default: undefined,
-  },
-});
-</script>
